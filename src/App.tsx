578d175cbde95f604cae228a5f9920b5b207aa65
hwalk1 <haydenjwalker1@gmail.com>
2023-06-19 19:30:20 +1000

    feat: first sign up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React,   { useEffect, useState } from 'react';
-import { createClient } from "@supabase/supabase-js";
+import React from 'react';
+import { createClient } from '@supabase/supabase-js';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './pages/Login';
 
-
-const supabase = createClient(process.env.REACT_APP_PROJECT_URL || '', process.env.REACT_APP_API_KEY || '');
+export const supabase = createClient(
+  process.env.REACT_APP_PROJECT_URL || '',
+  process.env.REACT_APP_API_KEY || ''
+);
 
 interface Country {
   id: number;
@@ -13,49 +17,34 @@ interface Country {
 type CountryList = Country[];
 
 interface CountryName {
-    name: string
+  name: string;
 }
 
 function App() {
-    const [countries, setCountries] = useState<CountryList>([]);
+  // const [countries, setCountries] = useState<CountryList>([]);
 
-    useEffect(() => {
-      getCountries();
-    }, []);
-    
-    async function getCountries() {
-        const { data } = await supabase.from("countries").select();
-        if (data !== null) {
-            setCountries(data as CountryList);
-        }
-      }
-      
-      return (
-        <ul>
-          {countries.map((country: CountryName) => (
-            <li key={country.name}>{country.name}</li>
-          ))}
-        </ul>
-      );
-    }
-    // return (
-    //     <div className="App">
-    //         <header className="App-header">
-    //             <img src={logo} className="App-logo" alt="logo" />
-    //             <p>
-    //                 Edit <code>src/App.tsx</code> and save to reload.
-    //             </p>
-    //             <a
-    //                 className="App-link"
-    //                 href="https://reactjs.org"
-    //                 target="_blank"
-    //                 rel="noopener noreferrer"
-    //             >
-    //                 Learn React
-    //             </a>
-    //         </header>
-    //     </div>
-    // );
-// }    
+  // useEffect(() => {
+  //   getCountries();
+  // }, []);
+
+  // async function getCountries() {
+  //     const { data } = await supabase.from("countries").select();
+  //     if (data !== null) {
+  //         setCountries(data as CountryList);
+  //     }
+  //   }
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <div>Hello world!</div>,
+    },
+    {
+      path: '/login',
+      element: <Login />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
 
 export default App;
