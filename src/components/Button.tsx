0be9bbee8ea32ea37940a6b76252df62b0505377
hwalk1 <haydenjwalker1@gmail.com>
2023-07-17 18:58:35 +1000

    Basic Styling of Login

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface IButton {
   title: string;
+  to?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-const Button = ({ title }: IButton) => {
-  return <button type="submit">{title}</button>;
+const Button = ({ title, to, onClick }: IButton) => {
+  if (to) {
+    return (
+      <Link to={to}>
+        <button type="submit">{title}</button>
+      </Link>
+    );
+  }
+  return (
+    <button type="submit" onClick={onClick}>
+      {title}
+    </button>
+  );
 };
 
 export default Button;
