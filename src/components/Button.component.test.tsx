8904db7160de8716186f225b132ac4642a8bd6ba
hwalk1 <haydenjwalker1@gmail.com>
2023-10-16 17:56:37 +1100

    Further Button Tests
    Co-authored-by: nick-cooney <nick-cooney@users.noreply.github.com>

--- src/components/Button.component.test.tsx
+++ src/components/Button.component.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from './Button';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 
 // Two potential buttons to render
@@ -51,5 +51,46 @@ describe('When testing our Button', () => {
     const button: HTMLElement = getByTestId('test-base_button');
     expect(button).toBeVisible();
     expect(button.textContent).toBe('Button');
+  });
+
+  // test button can be clicked
+  it('My Button is Clickable when enabled', () => {
+    const handleClick = jest.fn();
+    const { getByTestId } = render(
+      <Button title="Button" buttonType="primary" onClick={handleClick} />
+    );
+    const button: HTMLElement = getByTestId('test-base_button');
+    expect(button).toBeVisible();
+    fireEvent.click(button);
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('My Button is a Link when given a path', () => {
+    const route = '/';
+    const { getByTestId } = render(
+      <BrowserRouter>
+        <Button title="Button" buttonType="primary" to={route} />
+      </BrowserRouter>
+    );
+    const link: HTMLElement = getByTestId('test-nav_link');
+    expect(link).toBeVisible();
+    expect(link).toHaveAttribute('href', route);
+  });
+
+  // if the button is disabled, it can't be clicked
+  it('My Button is not Clickable when disabled', () => {
+    const handleClick = jest.fn();
+    const { getByTestId } = render(
+      <Button
+        title="Button"
+        buttonType="primary"
+        onClick={handleClick}
+        isDisabled
+      />
+    );
+    const button: HTMLElement = getByTestId('test-base_button');
+    expect(button).toBeVisible();
+    fireEvent.click(button);
+    expect(handleClick).toHaveBeenCalledTimes(0);
   });
 });
